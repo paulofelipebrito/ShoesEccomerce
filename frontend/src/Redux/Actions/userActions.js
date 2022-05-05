7b2095d27d16a96baca5d82365a2a555a1c6a5ca
Paulo Felipe Brito <paulofelipebrito@hotmail.com>
2022-05-05 17:22:16 -0300

    Feat: Register screen.

--- frontend/src/Redux/Actions/userActions.js
+++ frontend/src/Redux/Actions/userActions.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import {USER_LOGIN_REQUEST,USER_LOGIN_SUCCESS,USER_LOGIN_FAIL, USER_LOGOUT  } from '../Constants/UserContants';
+import {USER_LOGIN_REQUEST,USER_LOGIN_SUCCESS,USER_LOGIN_FAIL, USER_LOGOUT, USER_REGISTER_REQUEST, USER_REGISTER_SUCCESS,USER_REGISTER_FAIL  } from '../Constants/UserContants';
 
 //LOGIN
 export const login = (email,password) => async (dispatch) => {
@@ -42,4 +42,44 @@ export const logout = () => (dispatch) => {
   localStorage.removeItem("userInfo");
   dispatch({ type: USER_LOGOUT });
   //document.location.href= "/login";
+}
+
+//REGISTER
+export const register = (name, email,password) => async (dispatch) => {
+  try{
+    dispatch({
+      type: USER_REGISTER_REQUEST
+    })
+
+    const config = {
+      header: {
+        "Content-Type": "application/json"
+      },
+    }
+
+    const {data} = await axios.post(
+      `/api/users`,
+      { name, email,password}, 
+      config
+    );
+
+    dispatch({
+      type: USER_REGISTER_SUCCESS,
+      payload: data
+    })
+    dispatch({
+      type: USER_LOGIN_SUCCESS,
+      payload: data
+    });
+
+    localStorage.setItem("userInfo", JSON.stringify(data));
+  }catch(error){
+    dispatch({
+      type: USER_REGISTER_FAIL,
+      payload:
+        error.response && error.response.data.message 
+          ? error.response.data.message
+          : error.message, 
+    })
+  }
 }
