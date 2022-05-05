bdb3ae66067290ab7df2cd109668d34f85ea8580
Paulo Felipe Brito <paulofelipebrito@hotmail.com>
2022-05-05 08:33:17 -0300

    Feat: Register API.

--- server/Models/UserModel.js
+++ server/Models/UserModel.js
@@ -26,9 +26,20 @@ const userSchema = mongoose.Schema({
 }
 );
 
+// LOGIN
 userSchema.methods.comparePassword = async function (password) {
 	return await bcrypt.compare(password, this.password);
 }
+
+// REGISTER
+userSchema.pre("save",async function(next){
+  if(!this.isModified("password")){
+    next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt); 
+})
+
 const User = mongoose.model("User", userSchema);
 
 
