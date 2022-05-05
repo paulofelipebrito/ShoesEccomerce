1e392ab950687ba36c2d81c4f401caaaa18c7e82
Paulo Felipe Brito <paulofelipebrito@hotmail.com>
2022-05-05 16:51:20 -0300

    Feat: Logout redux.

--- frontend/src/Redux/Actions/userActions.js
+++ frontend/src/Redux/Actions/userActions.js
@@ -38,37 +38,8 @@ export const login = (email,password) => async (dispatch) => {
 }
 
 //LOGOUT
-export const logout = (email) => async (dispatch) => {
-  try{
-    dispatch({
-      type: USER_LOGOUT
-    })
-
-    const config = {
-      header: {
-        "Content-Type": "application/json"
-      },
-    }
-
-    const {data} = await axios.post(
-      `/api/users/login`,
-      {email}, 
-      config
-    );
-
-    dispatch({
-      type: USER_LOGIN_SUCCESS,
-      payload: data
-    })
-
-    localStorage.setItem("userInfo", JSON.stringify(data));
-  }catch(error){
-    dispatch({
-      type: USER_LOGIN_FAIL,
-      payload:
-        error.response && error.response.data.message 
-          ? error.response.data.message
-          : error.message, 
-    })
-  }
+export const logout = () => (dispatch) => {
+  localStorage.removeItem("userInfo");
+  dispatch({ type: USER_LOGOUT });
+  //document.location.href= "/login";
 }
